f7a5bfdd23be6c4e1be0b053737a3311b5f0084a
Bill DuGe <billduge11@gmail.com>
2018-09-23 22:58:29 -0400

    Reads date from html form
    Added functions to read the html date form (which registers as a string) into integer variables.

--- WeekdayFinder.js
+++ WeekdayFinder.js
@@ -1,4 +1,32 @@
+var date;
+var year;
+var month;
+var day;
 
-function printValue(){
-    console.log(document.getElementById("datePicker").value)
+function main(){
+    getDate()
 }
+
+function getDate(){
+   date = document.getElementById("datePicker").value
+   getYear()
+   getMonth()
+   getDay()
+}
+
+function getYear(){
+    yearString = date.charAt(0)+date.charAt(1)+date.charAt(2)+date.charAt(3)  
+    year = parseInt(yearString)
+}
+
+function getMonth(){
+    monthString = date.charAt(5)+date.charAt(6)
+    month = parseInt(monthString)
+}
+
+function getDay(){
+    dayString = date.charAt(8)+date.charAt(9)
+    day = parseInt(dayString)
+}
+
+
